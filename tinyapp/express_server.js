4f80e13bd5bf888b9bf80d684abfc716d4482190
AlannaParsons <alannagparsons@gmail.ca>
2023-07-24 19:35:24 +0000

    LL: w3d1: tiny app, url shortnener base

---
+++ tinyapp/express_server.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const app = express();
+const PORT = 8080; // default port 8080
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+// app.get("/", (req, res) => {
+//   res.send("Hello!");
+// });
+
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
+
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// });
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
+  res.render("urls_show", templateVars);
+});
+
+app.get("/set", (req, res) => {
+  const a = 1;
+  res.send(`a = ${a}`);
+ });
+
+ app.get("/fetch", (req, res) => {
+  res.send(`a = ${a}`);
+ });
+
+
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
+//https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
+function generateRandomString() {
+  const crypto = require("crypto");
+  const id = crypto.randomBytes(5).toString('hex');
+  console.log('RANDOM STR:',id);
+}
+
+generateRandomString()
