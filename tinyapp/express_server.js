6a5eff321bb11793367123dda399cbe40bb704f3
AlannaParsons <alannagparsons@gmail.ca>
2023-07-27 22:11:08 +0000

    LL: w3d1: tiny app mostly finished. may add  functions later

--- tinyapp/express_server.js
+++ tinyapp/express_server.js
@@ -1,3 +1,21 @@
+/*
+-------------------------------------------------------------
+Alanna Parsons
+Lighthouse labs
+Jul 24 2023
+-------------------------------------------------------------
+Instruction
+
+Add GET route to expressserver.js and render using accompanying template
+Add POST route to expressserver.js to receive form submission
+Update expressserver.js to store short-url and long-url key value pair to database when a POST request is received to /urls
+Update expressserver.js to respond with a redirect from /urls to /urls/:id
+Add validation
+
+- data base is wiped when server is shut down
+*/
+
+
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
@@ -9,18 +27,6 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
-// app.get("/", (req, res) => {
-//   res.send("Hello!");
-// });
-
-// app.get("/hello", (req, res) => {
-//   res.send("<html><body>Hello <b>World</b></body></html>\n");
-// });
-
-// app.get("/urls.json", (req, res) => {
-//   res.json(urlDatabase);
-// });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
@@ -35,32 +41,29 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-app.get("/urls/:id", (req, res) => {
+//add for invalid id??
+app.get("/u/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render("urls_show", templateVars);
 });
 
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
- });
-
- app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
- });
-
-
-
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+//validation. could add pop up error message for user, or change redirect
+  if (!Object.values(urlDatabase).includes(req.body.longURL)){
+    const shortURL = generateRandomString();
+    const goURL = `/u/${shortURL}`;
+
+    urlDatabase[shortURL] = req.body.longURL;
+    res.redirect(goURL);
+  } else {
+  //console.log('this url already exists');
+  res.redirect("/urls/new");
+  }
 });
 
 //https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
 function generateRandomString() {
   const crypto = require("crypto");
-  const id = crypto.randomBytes(5).toString('hex');
-  console.log('RANDOM STR:',id);
+  const id = crypto.randomBytes(4).toString('hex');
+  return id;
 }
-
-generateRandomString()
